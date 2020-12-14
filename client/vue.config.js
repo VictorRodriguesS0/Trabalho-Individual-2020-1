4e8385a4607673340d7692778ed687b0e99876b4
Victor Rodrigues <victorrsilva17@gmail.com>
2020-12-14 19:23:38 -0300

    Heroku 3

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   devServer: {
-    port: process.env.PORT || 8080
+    port: (process.env.PORT || 8080),
+    disableHostCheck: true
   },
   transpileDependencies: [
     'vuetify'
